9e5e361637b01422b08fe7a83746fcdfe4c75f05
Todd Miller <toddses@gmail.com>
2016-09-15 11:41:12 -0400

    add postgres support because heroku does not support sqlite

--- bin/bot.js
+++ bin/bot.js
@@ -9,7 +9,7 @@
 var PunBot = require('../lib/punbot');
 
 var token  = process.env.PUN_BOT_API_KEY;
-var dbPath = process.env.PUN_BOT_DB_PATH;
+var dbPath = process.env.DATABASE_URL;
 var name   = process.env.PUN_BOT_NAME;
 
 var punbot = new PunBot({
